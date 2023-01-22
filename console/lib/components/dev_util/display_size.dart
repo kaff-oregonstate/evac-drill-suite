c118b26ec8fed7bc7656b8d51fdff00cb843cb5d
David Kaff <72285138+kaff-oregonstate@users.noreply.github.com>
2023-01-21 16:15:43 -0800

    import prototype: `dev_util`

---
+++ console/lib/components/dev_util/display_size.dart
@@ -0,0 +1,52 @@
+import 'package:evac_drill_console/flutter_flow/flutter_flow_theme.dart';
+import 'package:flutter/material.dart';
+
+class DisplaySize extends StatelessWidget {
+  const DisplaySize({Key? key, required this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [child, SizeDisplay()],
+    );
+  }
+}
+
+class SizeDisplay extends StatelessWidget {
+  const SizeDisplay({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _width = MediaQuery.of(context).size.width;
+    final _height = MediaQuery.of(context).size.height;
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          color: FlutterFlowTheme.of(context).primaryBackground,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: FlutterFlowTheme.of(context).tertiaryColor,
+            width: 2,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                '${_width} x ${_height}',
+                style: FlutterFlowTheme.of(context).title3.override(
+                      fontFamily: 'Outfit',
+                      color: FlutterFlowTheme.of(context).secondaryText,
+                    ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
