f1fe7688165f073aedf5b5ab74f9d3593414a502
David Kaff <72285138+kaff-oregonstate@users.noreply.github.com>
2023-01-21 16:17:38 -0800

    import prototype: `drill_task_fields`

---
+++ console/lib/components/drill_task_fields/req_loc_perms_task_field.dart
@@ -0,0 +1,143 @@
+import 'package:evac_drill_console/flutter_flow/flutter_flow_theme.dart';
+import 'package:flutter/material.dart';
+
+class ReqLocPermsTaskField extends StatefulWidget {
+  const ReqLocPermsTaskField({Key? key}) : super(key: key);
+
+  @override
+  State<ReqLocPermsTaskField> createState() => _ReqLocPermsTaskFieldState();
+}
+
+class _ReqLocPermsTaskFieldState extends State<ReqLocPermsTaskField> {
+  TextEditingController? textController;
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    textController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: FlutterFlowTheme.of(context).primaryBackground,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: FlutterFlowTheme.of(context).tertiaryColor,
+            width: 2,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(0, 16, 0, 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    SelectionArea(
+                        child: Text(
+                      'Request Location Permissions',
+                      style: FlutterFlowTheme.of(context).title3.override(
+                            fontFamily: 'Outfit',
+                            color: FlutterFlowTheme.of(context).secondaryText,
+                          ),
+                    )),
+                    const Icon(
+                      Icons.close_rounded,
+                      color: Color(0xBF95A1AC),
+                      size: 24,
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                height: 24,
+                thickness: 2,
+                indent: 12,
+                endIndent: 12,
+                color: FlutterFlowTheme.of(context).tertiaryColor,
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(16, 4, 16, 4),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
+                      child: SelectionArea(
+                          child: Text(
+                        'Title:',
+                        style: FlutterFlowTheme.of(context).bodyText1,
+                      )),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        controller: textController,
+                        autofocus: true,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          hintText: '[Allow Researchers to Track Location…]',
+                          hintStyle: FlutterFlowTheme.of(context).bodyText2,
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          errorBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedErrorBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          filled: true,
+                          fillColor:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                        ),
+                        style: FlutterFlowTheme.of(context).bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ console/lib/components/drill_task_fields/travel_task_field.dart
@@ -0,0 +1,202 @@
+// // import 'package:evac_drill_console/flutter_flow/flutter_flow_place_picker.dart';
+// import 'package:evac_drill_console/flutter_flow/flutter_flow_theme.dart';
+// import 'package:evac_drill_console/flutter_flow/flutter_flow_widgets.dart';
+// // import 'package:evac_drill_console/flutter_flow/place.dart';
+// import 'package:flutter/material.dart';
+
+// class TravelTaskField extends StatefulWidget {
+//   const TravelTaskField({Key? key}) : super(key: key);
+
+//   @override
+//   _TravelTaskFieldState createState() => _TravelTaskFieldState();
+// }
+
+// class _TravelTaskFieldState extends State<TravelTaskField> {
+//   TextEditingController? textController1;
+//   TextEditingController? textController2;
+//   var placePickerValue = FFPlace();
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     textController1 = TextEditingController();
+//     textController2 = TextEditingController();
+//     WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+//   }
+
+//   @override
+//   void dispose() {
+//     textController1?.dispose();
+//     textController2?.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
+//       child: Container(
+//         width: double.infinity,
+//         decoration: BoxDecoration(
+//           color: FlutterFlowTheme.of(context).primaryBackground,
+//           borderRadius: BorderRadius.circular(12),
+//           border: Border.all(
+//             color: FlutterFlowTheme.of(context).secondaryBackground,
+//             width: 2,
+//           ),
+//         ),
+//         child: Column(
+//           mainAxisSize: MainAxisSize.max,
+//           children: [
+//             Padding(
+//               padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
+//               child: Row(
+//                 mainAxisSize: MainAxisSize.max,
+//                 children: [
+//                   Padding(
+//                     padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
+//                     child: SelectionArea(
+//                         child: Text(
+//                       'Location name:',
+//                       style: FlutterFlowTheme.of(context).bodyText1,
+//                     )),
+//                   ),
+//                   Expanded(
+//                     child: TextFormField(
+//                       controller: textController1,
+//                       autofocus: true,
+//                       obscureText: false,
+//                       decoration: InputDecoration(
+//                         hintText: '[Some hint text...]',
+//                         hintStyle: FlutterFlowTheme.of(context).bodyText2,
+//                         enabledBorder: UnderlineInputBorder(
+//                           borderSide: const BorderSide(
+//                             color: Color(0x00000000),
+//                             width: 1,
+//                           ),
+//                           borderRadius: BorderRadius.circular(8),
+//                         ),
+//                         focusedBorder: UnderlineInputBorder(
+//                           borderSide: const BorderSide(
+//                             color: Color(0x00000000),
+//                             width: 1,
+//                           ),
+//                           borderRadius: BorderRadius.circular(8),
+//                         ),
+//                         errorBorder: UnderlineInputBorder(
+//                           borderSide: const BorderSide(
+//                             color: Color(0x00000000),
+//                             width: 1,
+//                           ),
+//                           borderRadius: BorderRadius.circular(8),
+//                         ),
+//                         focusedErrorBorder: UnderlineInputBorder(
+//                           borderSide: const BorderSide(
+//                             color: Color(0x00000000),
+//                             width: 1,
+//                           ),
+//                           borderRadius: BorderRadius.circular(8),
+//                         ),
+//                         filled: true,
+//                         fillColor:
+//                             FlutterFlowTheme.of(context).secondaryBackground,
+//                       ),
+//                       style: FlutterFlowTheme.of(context).bodyText1,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             Padding(
+//               padding: const EdgeInsetsDirectional.fromSTEB(16, 8, 16, 8),
+//               child: Row(
+//                 mainAxisSize: MainAxisSize.max,
+//                 children: [
+//                   Padding(
+//                     padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
+//                     child: SelectionArea(
+//                         child: Text(
+//                       'Location name:',
+//                       style: FlutterFlowTheme.of(context).bodyText1,
+//                     )),
+//                   ),
+//                   Expanded(
+//                     child: TextFormField(
+//                       controller: textController2,
+//                       autofocus: true,
+//                       obscureText: false,
+//                       decoration: InputDecoration(
+//                         hintText: '[Some hint text...]',
+//                         hintStyle: FlutterFlowTheme.of(context).bodyText2,
+//                         enabledBorder: UnderlineInputBorder(
+//                           borderSide: const BorderSide(
+//                             color: Color(0x00000000),
+//                             width: 1,
+//                           ),
+//                           borderRadius: BorderRadius.circular(8),
+//                         ),
+//                         focusedBorder: UnderlineInputBorder(
+//                           borderSide: const BorderSide(
+//                             color: Color(0x00000000),
+//                             width: 1,
+//                           ),
+//                           borderRadius: BorderRadius.circular(8),
+//                         ),
+//                         errorBorder: UnderlineInputBorder(
+//                           borderSide: const BorderSide(
+//                             color: Color(0x00000000),
+//                             width: 1,
+//                           ),
+//                           borderRadius: BorderRadius.circular(8),
+//                         ),
+//                         focusedErrorBorder: UnderlineInputBorder(
+//                           borderSide: const BorderSide(
+//                             color: Color(0x00000000),
+//                             width: 1,
+//                           ),
+//                           borderRadius: BorderRadius.circular(8),
+//                         ),
+//                         filled: true,
+//                         fillColor:
+//                             FlutterFlowTheme.of(context).secondaryBackground,
+//                       ),
+//                       style: FlutterFlowTheme.of(context).bodyText1,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             FlutterFlowPlacePicker(
+//               iOSGoogleMapsApiKey: '',
+//               androidGoogleMapsApiKey: '',
+//               webGoogleMapsApiKey: '',
+//               onSelect: (place) async {
+//                 setState(() => placePickerValue = place);
+//               },
+//               defaultText: 'Select Location',
+//               icon: const Icon(
+//                 Icons.place,
+//                 color: Colors.white,
+//                 size: 16,
+//               ),
+//               buttonOptions: FFButtonOptions(
+//                 width: 200,
+//                 height: 40,
+//                 color: FlutterFlowTheme.of(context).primaryColor,
+//                 textStyle: FlutterFlowTheme.of(context).subtitle2.override(
+//                       fontFamily: 'Space Grotesk',
+//                       color: Colors.white,
+//                     ),
+//                 borderSide: const BorderSide(
+//                   color: Colors.transparent,
+//                   width: 1,
+//                 ),
+//                 borderRadius: BorderRadius.circular(12),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
