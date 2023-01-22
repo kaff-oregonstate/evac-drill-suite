4738d96aaf80b8544c390978dbb27ed0d7821b4e
David Kaff <72285138+kaff-oregonstate@users.noreply.github.com>
2023-01-21 16:17:57 -0800

    import prototype: `survey_step_fields`

---
+++ console/lib/components/survey_step_fields/single_choice_question_step_field_widget.dart
@@ -0,0 +1,418 @@
+import 'package:evac_drill_console/flutter_flow/flutter_flow_theme.dart';
+import 'package:flutter/material.dart';
+
+class SingleChoiceQuestionStepFieldWidget extends StatefulWidget {
+  const SingleChoiceQuestionStepFieldWidget({Key? key}) : super(key: key);
+
+  @override
+  State<SingleChoiceQuestionStepFieldWidget> createState() =>
+      _SingleChoiceQuestionStepFieldWidgetState();
+}
+
+class _SingleChoiceQuestionStepFieldWidgetState
+    extends State<SingleChoiceQuestionStepFieldWidget> {
+  TextEditingController? textController1;
+  TextEditingController? textController2;
+  TextEditingController? textController3;
+  bool? checkboxValue1;
+  TextEditingController? textController4;
+  bool? checkboxValue2;
+  String? radioValue;
+
+  @override
+  void initState() {
+    super.initState();
+    textController1 = TextEditingController();
+    textController2 = TextEditingController();
+    textController3 = TextEditingController();
+    textController4 = TextEditingController();
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    textController1?.dispose();
+    textController2?.dispose();
+    textController3?.dispose();
+    textController4?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: FlutterFlowTheme.of(context).primaryBackground,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: FlutterFlowTheme.of(context).tertiaryColor,
+            width: 2,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(0, 16, 0, 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    SelectionArea(
+                        child: Text(
+                      'Single Choice Question',
+                      style: FlutterFlowTheme.of(context).title3.override(
+                            fontFamily: 'Outfit',
+                            color: FlutterFlowTheme.of(context).secondaryText,
+                          ),
+                    )),
+                    const Icon(
+                      Icons.close_rounded,
+                      color: Color(0xBF95A1AC),
+                      size: 24,
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                height: 24,
+                thickness: 2,
+                indent: 12,
+                endIndent: 12,
+                color: FlutterFlowTheme.of(context).tertiaryColor,
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(16, 4, 16, 8),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
+                      child: SelectionArea(
+                          child: Text(
+                        'Title:',
+                        style: FlutterFlowTheme.of(context).bodyText1,
+                      )),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        controller: textController1,
+                        autofocus: true,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          hintText:
+                              '[Do you prefer to vacation at the beach or the mountains?…]',
+                          hintStyle: FlutterFlowTheme.of(context).bodyText2,
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          errorBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedErrorBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          filled: true,
+                          fillColor:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                        ),
+                        style: FlutterFlowTheme.of(context).bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(16, 4, 16, 8),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 120,
+                      decoration: const BoxDecoration(),
+                      child: Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 8, 8, 0),
+                        child: SelectionArea(
+                            child: Text(
+                          'Text [optional]:',
+                          style: FlutterFlowTheme.of(context).bodyText1,
+                        )),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        controller: textController2,
+                        autofocus: true,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          hintText: '[Yes, you have to pick one. …]',
+                          hintStyle: FlutterFlowTheme.of(context).bodyText2,
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          errorBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedErrorBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          filled: true,
+                          fillColor:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                        ),
+                        style: FlutterFlowTheme.of(context).bodyText1,
+                        maxLines: 5,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(16, 4, 16, 8),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Container(
+                      width: 120,
+                      decoration: const BoxDecoration(),
+                      child: Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
+                        child: SelectionArea(
+                            child: Text(
+                          'First Choice:',
+                          style: FlutterFlowTheme.of(context).bodyText1,
+                        )),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        controller: textController3,
+                        autofocus: true,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          hintText: '[The beach…]',
+                          hintStyle: FlutterFlowTheme.of(context).bodyText2,
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          errorBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedErrorBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          filled: true,
+                          fillColor:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                        ),
+                        style: FlutterFlowTheme.of(context).bodyText1,
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(8, 0, 0, 0),
+                      child: SelectionArea(
+                          child: Text(
+                        'Default?:',
+                        style: FlutterFlowTheme.of(context).bodyText1.override(
+                              fontFamily: 'Space Grotesk',
+                              color: FlutterFlowTheme.of(context).secondaryText,
+                            ),
+                      )),
+                    ),
+                    Theme(
+                      data: ThemeData(
+                        checkboxTheme: CheckboxThemeData(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0),
+                          ),
+                        ),
+                        unselectedWidgetColor: const Color(0xFFF5F5F5),
+                      ),
+                      child: Checkbox(
+                        value: checkboxValue1 ??= true,
+                        onChanged: (newValue) async {
+                          setState(() => checkboxValue1 = newValue!);
+                        },
+                        activeColor: FlutterFlowTheme.of(context).primaryColor,
+                      ),
+                    ),
+                    Radio(
+                      value: 'first',
+                      groupValue: radioValue,
+                      onChanged: (newValue) async {
+                        setState(() => radioValue = newValue);
+                      },
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(16, 4, 16, 8),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Container(
+                      width: 120,
+                      decoration: const BoxDecoration(),
+                      child: Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
+                        child: SelectionArea(
+                            child: Text(
+                          'Second Choice:',
+                          style: FlutterFlowTheme.of(context).bodyText1,
+                        )),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        controller: textController4,
+                        autofocus: true,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          hintText: '[The mountains…]',
+                          hintStyle: FlutterFlowTheme.of(context).bodyText2,
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          errorBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedErrorBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          filled: true,
+                          fillColor:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                        ),
+                        style: FlutterFlowTheme.of(context).bodyText1,
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(8, 0, 0, 0),
+                      child: SelectionArea(
+                          child: Text(
+                        'Default?:',
+                        style: FlutterFlowTheme.of(context).bodyText1.override(
+                              fontFamily: 'Space Grotesk',
+                              color: FlutterFlowTheme.of(context).secondaryText,
+                            ),
+                      )),
+                    ),
+                    Theme(
+                      data: ThemeData(
+                        checkboxTheme: CheckboxThemeData(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0),
+                          ),
+                        ),
+                        unselectedWidgetColor:
+                            FlutterFlowTheme.of(context).secondaryText,
+                      ),
+                      child: Checkbox(
+                        value: checkboxValue2 ??= false,
+                        onChanged: (newValue) async {
+                          setState(() => checkboxValue2 = newValue!);
+                        },
+                        activeColor: FlutterFlowTheme.of(context).primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
