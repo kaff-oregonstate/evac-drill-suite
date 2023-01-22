dcb90b7edf84a44a1d9c0118a5048a27c7f66e85
David Kaff <72285138+kaff-oregonstate@users.noreply.github.com>
2023-01-21 16:28:14 -0800

    import prototype: `index`

---
+++ console/lib/index.dart
@@ -0,0 +1,8 @@
+// Export pages
+export 'pages/sign_in_page.dart' show SignInPage;
+export 'pages/main_planned_drills_page.dart' show MainPlannedDrillsPage;
+export 'pages/plan_drill_page.dart' show PlanDrillPage;
+export 'pages/final_review_page.dart' show FinalReviewPage;
+export 'pages/main_published_drills_page.dart' show MainPublishedDrillsPage;
+export 'pages/main_completed_drills_page.dart' show MainCompletedDrillsPage;
+export 'pages/main_team_members_page.dart' show MainTeamMembersPage;
