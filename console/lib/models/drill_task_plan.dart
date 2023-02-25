7ff852b14db24e1036982ebf934ff24bd63d2a6b
David Kaff <72285138+kaff-oregonstate@users.noreply.github.com>
2023-02-24 18:17:39 -0800

    Console 0.2.0 DrillTaskPlan Model

--- console/lib/models/drill_task_plan.dart
+++ console/lib/models/drill_task_plan.dart
@@ -11,25 +11,131 @@ enum DrillTaskType {
   upload,
 }
 
+extension DrillTaskName on DrillTaskType {
+  String get fullName {
+    switch (this) {
+      case DrillTaskType.survey:
+        return 'Survey';
+      case DrillTaskType.reqLocPerms:
+        return 'Request Location Permissions';
+      case DrillTaskType.practiceEvac:
+        return 'Practice Evacuation';
+      case DrillTaskType.travel:
+        return 'Travel';
+      case DrillTaskType.upload:
+        return 'Upload';
+      default:
+        throw Exception('Internal: bad DrillTaskType on .fullName');
+    }
+  }
+}
+
+// FIXME: The double-layers of abstraction (DrillTask, TaskDetails) seems redundant
+// and unnecessary now, why isn't just DrillTask with implemented types? Let's
+// switch this once the initial version of the Console is done.
+
 /// The DrillTaskPlan describes within the DrillPlan the current plan for a
 /// matching DrillTask object within a DrillDetails object
 
 class DrillTaskPlan {
   final String taskID;
-  final int index;
+  // final int index;
   final DrillTaskType taskType;
-  final String title;
-  final TaskDetailsPlan details;
+  String? title;
+  TaskDetailsPlan details;
 
   DrillTaskPlan({
     required this.taskID,
-    required this.index,
+    // required this.index,
     required this.taskType,
-    required this.title,
+    this.title,
     required this.details,
   });
 
-  // TODO: DrillTaskPlan.fromJson(json)
-  // TODO: DrillTaskPlan.toJson()
-  // TODO: recursive DrillTaskPlan.paramsMissing()
+  factory DrillTaskPlan.fromJson(Map<String, dynamic> json) {
+    DrillTaskType thisTaskType;
+    switch (json['taskType']) {
+      case 'survey':
+        thisTaskType = DrillTaskType.survey;
+        break;
+      case 'reqLocPerms':
+        thisTaskType = DrillTaskType.reqLocPerms;
+        break;
+      case 'practiceEvac':
+        thisTaskType = DrillTaskType.practiceEvac;
+        break;
+      case 'travel':
+        thisTaskType = DrillTaskType.travel;
+        break;
+      case 'upload':
+        thisTaskType = DrillTaskType.upload;
+        break;
+      default:
+        throw FormatException(
+            'Unrecognized DrillTaskType: ${json['taskType']}');
+    }
+    return DrillTaskPlan(
+      taskID: json['taskID'],
+      // index: json['index'],
+      taskType: thisTaskType,
+      title: json['title'],
+      details: TaskDetailsPlan.fromJson(json),
+    );
+  }
+
+  // @override
+  // bool operator ==(Object other) {
+  //   if (other.runtimeType != DrillTaskPlan) {
+  //     print('other.runtimeType');
+  //     return false;
+  //   }
+  //   other as DrillTaskPlan;
+  //   // taskID
+  //   if (taskID != other.taskID) {
+  //     print('taskID');
+  //     return false;
+  //   }
+  //   // taskType
+  //   if (taskType != other.taskType) {
+  //     print('taskType');
+  //     return false;
+  //   }
+  //   // title
+  //   if (title != other.title) {
+  //     print('title');
+  //     return false;
+  //   }
+  //   // details
+  //   if (details != other.details) {
+  //     print('details');
+  //     return false;
+  //   }
+
+  //   return true;
+  // }
+
+  @override
+  String toString() => toJson().toString();
+
+  Map<String, dynamic> toJson() => {
+        'taskID': taskID,
+        // 'index': index,
+        'taskType': taskType.name,
+        'title': title,
+        'details': details.toJson(),
+      };
+
+  List<MissingPlanParam> paramsMissing() {
+    List<MissingPlanParam> missingParams = [];
+
+    if (title == null || title!.isEmpty) {
+      missingParams.add(MissingPlanParam('task.title'));
+    }
+    // details (recursive)
+    for (final missingParam in details.paramsMissing()) {
+      missingParams.add(MissingPlanParam(missingParam.field));
+    }
+
+    return missingParams;
+  }
 }
